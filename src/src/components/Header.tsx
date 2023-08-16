8ad7a25cfb48274fca57706fe1cfa502a2c678d0
kiran-evans <thekiranevans@gmail.com>
2023-08-16 12:32:50 +0100

    Changed popover for conditionally-rendered links

--- src/src/components/Header.tsx
+++ src/src/components/Header.tsx
@@ -1,42 +1,42 @@
 import { AccountCircle, Search, ShoppingCart } from '@mui/icons-material';
-import { Collapse } from '@mui/material';
-import { useState } from 'react';
-import { AccountPopover } from './AccountPopover';
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AppContext } from './ContextProvider';
 
 export const Header = () => {
 
-    const [popoverIsOpen, setPopoverIsOpen] = useState(false);
+    const { state } = useContext(AppContext);
 
     return (
-        <>
-            <header>
-                <div id="logoContainer">
-                    Kloth
-                </div>
+        <header>
+            <div id="logoContainer">
+                <Link to='/'>Kloth</Link>
+            </div>
 
-                <nav id="categoryNav">
-                    {/* Get list of categories and show them as a list here */}
-                </nav>
+            <nav id="categoryNav">
+                {/* Get list of categories and show them as a list here */}
+            </nav>
 
-                <div id="searchContainer">
-                    <Search sx={{ fontSize: "1.5em" }} />
-                    <input id="search" type="search" placeholder="Search..." />
-                </div>
+            <div id="searchContainer">
+                <Search sx={{ fontSize: "1.5em" }} />
+                <input id="search" type="search" placeholder="Search..." />
+            </div>
 
-                <div id="iconsContainer">
-                    <div id="cartIcon">
-                        <ShoppingCart sx={{ fontSize: "1em" }} />
-                    </div>
-                    <div id="accountIcon" onClick={() => setPopoverIsOpen(!popoverIsOpen)}>
+            <div id="iconsContainer">
+                {/* Only render the shopping cart if the user is logged in */}
+                {state.user && 
+                    <Link to='/cart' title='Cart'>
+                        <div id="cartIcon">
+                            <ShoppingCart sx={{ fontSize: "1em" }} />
+                        </div>
+                    </Link>
+                }
+                <Link to={state.user ? '/account' : '/login'} title={state.user ? 'Your Account' : 'Login / Create Account'}>
+                    <div id="accountIcon">
                         <AccountCircle sx={{ fontSize: "1em" }} />
                     </div>
-                </div>
-            </header>
-
-            {/* Popover */}
-            <Collapse in={popoverIsOpen}>
-                <AccountPopover />
-            </Collapse>
-        </>
+                </Link>
+            </div>
+        </header>
     )
 }
