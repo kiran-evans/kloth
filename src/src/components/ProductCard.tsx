1cdbb761a1ecd53ac14705cba16eb2f7fe2d8e07
kiran-evans <thekiranevans@gmail.com>
2023-08-29 14:09:16 +0100

    Changed imageUrl to img_url in ProductCard.tsx

--- src/src/components/ProductCard.tsx
+++ src/src/components/ProductCard.tsx
@@ -6,7 +6,7 @@ export const ProductCard = (props: Product) => {
     return (
         <Link to={`/product/${props.id}`}>
             <div className="productCard">
-                <img src={props.imageUrl} />
+                <img src={props.img_url} />
                 <div className="productInfo">
                     <div className="infoText">
                         <p className="productName">{props.name}</p>
